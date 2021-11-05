b6ddd784b586f62967542d80381ec93e9879bacf
Lukas Alois Zbornik <l.a.zbornik@gmail.com>
2021-11-05 08:23:25 +0100

    Rock Paper Scissors

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # lazbo-java
 ## Fibonacci Sequence
 [Fibonacci sequence](https://en.wikipedia.org/wiki/Fibonacci_number) is sequence of numbers, such that each number is the sum of two preceding ones, starting from 0 and 1. My code will ask you how many numbers you want to show and will do the rest.
+
+## Rock Paper Scissors
+[Rock Paper Scissors](https://en.wikipedia.org/wiki/Rock_paper_scissors) is hand game usually played between two people, in wich players simultaneously forms one of three shapes. In my program shapes are represented by numbers and player will input them Rock(1), Paper(2), Scissors(3) and computer will also generate random number. After numbers are selected program will evaluate winner.
---
+++ Rock Paper Scissors/mainRocPapSci.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import java.util.Random;
+public class mainRocPapSci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rn = new Random();
+
+        int playerSel;
+        do{
+            System.out.print("Paper(1) \nRock(2) \nScissors(3) > ");
+            playerSel = sc.nextInt();
+        }while(playerSel < 1 || playerSel > 3);
+        int computerSel = rn.nextInt(4)+1;
+        if(computerSel == 1 && playerSel == 2 || computerSel == 2 && playerSel == 3 || computerSel == 3 && playerSel == 1){
+            System.out.println("Computer has won!");
+        } else if (computerSel == playerSel){
+            System.out.println("It's a tie!");
+        } else {
+            System.out.println("Player has won");
+        }
+
+        System.out.print("Wanna play again?(y/n) >");
+        Character again = sc.next().toLowerCase().charAt(0);
+        sc.close();
+        if(again.equals('y')){
+            main(args);
+        }
+    }
+}
